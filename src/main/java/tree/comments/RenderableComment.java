fe63f55480aa1a89f93b3602dac43d99ab1e6426
owen Bodley <owen@biomatters.com>
2016-01-21 13:38:56 +1300

    ShowChildComments
     * Made child comments show up

--- src/main/java/tree/comments/RenderableComment.java
+++ src/main/java/tree/comments/RenderableComment.java
@@ -8,11 +8,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
 public class RenderableComment {
 
     static final int commentHeight = 50;
     static final int padding = 10;
+    static final int subCommentsHeight = 50;
 
     private Comment comment;
 
@@ -26,8 +28,11 @@ public class RenderableComment {
 
     private JBLabel label;
 
+    private ArrayList<JBLabel> childCommentLabels;
+
     public RenderableComment(Comment comment) {
         this.comment = comment;
+        this.childCommentLabels = new ArrayList<JBLabel>();
         try {
             this.label = new JBLabel("<html><b>" + comment.getAuthorInfo().getDisplayName() + ": " + comment.getLineNumber() + "</b><br>" + comment.getContentRendered() + "</html>");
         } catch (NullPointerException ex) {
@@ -39,6 +44,13 @@ public class RenderableComment {
             IconLoader.setIcon(comment.getAuthorInfo().getAvatar(), label);
             label.setHorizontalAlignment(SwingConstants.LEFT);
             label.setVerticalTextPosition(SwingConstants.TOP);
+        }
+        for (Comment child : comment.getChildren()) {
+            try {
+                childCommentLabels.add(new JBLabel("<html><b>" + child.getAuthorInfo().getDisplayName() + "</b><br>" + child.getContentRendered() + "</html>"));
+            } catch (NullPointerException ex) {
+                childCommentLabels.add(new JBLabel("<html>" + child.getContentRendered() + "</html>"));
+            }
         }
     }
 
@@ -97,11 +109,12 @@ public class RenderableComment {
      * This should be used for the Y bounds only - X bounds are dependent on container width
      */
     public Rectangle getBounds() {
-        return new Rectangle(2*padding, y-label.getHeight()/2, 3*padding, label.getPreferredSize().height+padding);
+        return new Rectangle(2 * padding, y - label.getHeight() / 2, 3 * padding, label.getPreferredSize().height + padding);
     }
 
     public void paint(Graphics2D g, int containerWidth) {
-        int height = label.getPreferredSize().height+padding;
+        int labelHeight = label.getPreferredSize().height;
+        int height = labelHeight + padding + subCommentsHeight;
         float lineWidth = cursorInLine ? 2.0f : 1.0f;
         Color backgroundColor = cursorInLine ? new JBColor(0xe5f1ff, 0x344b67) : JBColor.white;
         Color borderColor = JBColor.gray;
@@ -111,34 +124,56 @@ public class RenderableComment {
         Shape commentBubble = new RoundRectangle2D.Double(2 * padding, y - height / 2, containerWidth - 3 * padding, height, 15, 15);
         Polygon commentArrow = new Polygon(new int[]{padding / 2, 2 * padding + 1, 2 * padding + 1}, new int[]{lineY, y - padding / 2, y + padding / 2}, 3);
 
-
-        //draw the bubble...
+        // Draw the bubble.
         g.setColor(backgroundColor);
         g.fill(commentBubble);
         g.setColor(borderColor);
         g.draw(commentBubble);
 
-        //draw the arrow
+        // Draw the arrow.
         g.setColor(backgroundColor);
         g.draw(commentArrow);
         g.fill(commentArrow);
         g.setColor(JBColor.GRAY);
-        g.drawLine(commentArrow.xpoints[0], commentArrow.ypoints[0], commentArrow.xpoints[1]-1, commentArrow.ypoints[1]);
-        g.drawLine(commentArrow.xpoints[0], commentArrow.ypoints[0], commentArrow.xpoints[2]-1, commentArrow.ypoints[2]);
+        g.drawLine(commentArrow.xpoints[0], commentArrow.ypoints[0], commentArrow.xpoints[1] - 1, commentArrow.ypoints[1]);
+        g.drawLine(commentArrow.xpoints[0], commentArrow.ypoints[0], commentArrow.xpoints[2] - 1, commentArrow.ypoints[2]);
+
+        int childOffset = 0;
+        int childHeight = 50;
+        for (JBLabel child : childCommentLabels) {
+            int childLabelHeight = child.getPreferredSize().height;
+            Shape childBubble = new RoundRectangle2D.Double(2 * padding, y + height / 2 + childOffset, containerWidth - 3 * padding, childLabelHeight + padding, 15, 15);
+            childOffset += childLabelHeight;
+            // Draw the bubble.
+            g.setColor(backgroundColor);
+            g.fill(childBubble);
+            g.setColor(borderColor);
+            g.draw(childBubble);
+
+            // Draw the text label.
+            child.setSize(new Dimension(containerWidth - 5 * padding, childHeight - 2 * padding));
+            int labelCentreX = 3 * padding;
+            int labelCentreY = y + height / 2 + childOffset / 2 - padding;
+            g.translate(labelCentreX, labelCentreY);
+            child.paint(g);
+            g.translate(-labelCentreX, -labelCentreY);
+        }
 
 
-        //hack to scale user icons to reasonable size
-        if(label.getIcon() != null && label.getIcon() instanceof ImageIcon) {
-            ImageIcon imageIcon = ((ImageIcon)label.getIcon());
-            if(imageIcon.getImage().getWidth(null) > 32) {
+        // Hack to scale user icons to reasonable size.
+        if (label.getIcon() != null && label.getIcon() instanceof ImageIcon) {
+            ImageIcon imageIcon = ((ImageIcon) label.getIcon());
+            if (imageIcon.getImage().getWidth(null) > 32) {
                 label.setIcon(new ImageIcon(imageIcon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH)));
             }
         }
 
-        //text for the bubble...
-        label.setSize(new Dimension(containerWidth-5*padding, commentHeight-2*padding));
-        g.translate(3*padding, y-commentHeight/2+padding);
+        // Text for the bubble.
+        label.setSize(new Dimension(containerWidth - 5 * padding, labelHeight - 2 * padding));
+        int labelCentreX = 3 * padding;
+        int labelCentreY = y - height / 2 + 2 * padding;
+        g.translate(labelCentreX, labelCentreY);
         label.paint(g);
-        g.translate(-3*padding, -y+commentHeight/2-padding);
+        g.translate(-labelCentreX, -labelCentreY);
     }
 }
